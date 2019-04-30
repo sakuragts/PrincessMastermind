ed37b5a046efee693bc9ce62385de1b68541c85a
sakuragts <BoboboPuniPotato44>
2019-04-30 17:08:16 -0400

    coded secret code generator

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,71 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.Random;
 
 public class Main {
 
     public static void main (String[] args) {
+        Token [] secretCode = null;
+        Token singleToken;
+        String tokenColor;
+        int tokenPosition;
 
-        testImage();
+        secretCode = secretCode();
+        singleToken = secretCode[0];
+
+        System.out.println(singleToken.getPosition());
 
 
     }
+
+    public static Token [] secretCode() {
+        Token [] secretCode = {null, null, null, null};
+        int [] numberCode;
+        int tokenColor;
+        int position;
+        Token token;
+        Random randomColor = new Random();
+
+        numberCode = produceCode();
+
+
+        for(int i = 0; i < 4; i++) {
+            position = numberCode[i];
+            tokenColor = randomColor.nextInt(5);
+            token = new Token(tokenColor, position);
+            secretCode[i] = token;
+        }
+        return secretCode;
+    }
+
+    public static int [] produceCode() {
+        int [] code = {-1, -1, -1, -1};
+        int number = 0;
+        int codeNumber;
+        Random randomNum = new Random();
+
+        while(number < 4){
+            codeNumber = randomNum.nextInt(4);
+            if(!contains(code, codeNumber)) {
+                code[number] = codeNumber;
+                number++;
+            }
+        }
+        return code;
+    }
+
+    private static boolean contains(int [] list, int number) {
+        boolean doesContain = false;
+
+        for(int i = 0; i < list.length; i++) {
+            if(number == list[i]) {
+                doesContain = true;
+            }
+        }
+        return doesContain;
+    }
+
 
     public static void testImage() {
         File testImage = null;
--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,28 @@
+
+
 public class Token {
 
     private String [] colors = {"blue", "yellow", "green", "red", "orange"};
-    private int [] position;
+    private int position;
+    private String tokenColor;
+
+    public Token (int color, int position) {
+        if (color >= 0 && color < 5) {
+            this.tokenColor = colors[color];
+        }
+        if(position >= 0 && position < 4) {
+            this.position = position;
+        }
+    }
+
+    public String getTokenColor() {
+        return this.tokenColor;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+
+
 }
