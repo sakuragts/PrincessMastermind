1b78bfcbdf3463b54ce4e54f43550a155f6ba68c
sakuragts <BoboboPuniPotato44>
2019-06-05 17:36:45 -0400

    modified Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.Random;
+
 import java.util.Scanner;
 
 public class Main {
@@ -15,72 +11,10 @@ public class Main {
         String [] userAnswer = new String[4];
         int tokenPosition;
 
+        printAnswer(secretCode, userAnswer);
 
-        secretCode.secretCode();
-        secretCode.getCodeWithTokens();
-
-        for(int i = 1; i < 5; i++)
-        {
-            userAnswer = userInput(i, userAnswer);
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            tokenColor = userAnswer[i];
-            System.out.print(tokenColor + ", ");
-        }
-        System.out.println("\n" +
-                secretCode);
-        System.out.println(secretCode.getSingleToken(3));
+        //System.out.println(secretCode.getSingleToken(3));
     }
-/*
-    public static Token [] secretCode() {
-        Token [] secretCode = {null, null, null, null};
-        int [] numberCode;
-        int tokenColor;
-        int position;
-        Token token;
-        Random randomColor = new Random();
-
-        numberCode = produceCode();
-
-
-        for(int i = 0; i < 4; i++) {
-            position = numberCode[i];
-            tokenColor = randomColor.nextInt(5);
-            token = new Token(tokenColor, position);
-            secretCode[i] = token;
-        }
-        return secretCode;
-    }
-
-    public static int [] produceCode() {
-        int [] code = {-1, -1, -1, -1};
-        int number = 0;
-        int codeNumber;
-        Random randomNum = new Random();
-
-        while(number < 4){
-            codeNumber = randomNum.nextInt(4);
-            if(!contains(code, codeNumber)) {
-                code[number] = codeNumber;
-                number++;
-            }
-        }
-        return code;
-    }
-
-    private static boolean contains(int [] list, int number) {
-        boolean doesContain = false;
-
-        for(int i = 0; i < list.length; i++) {
-            if(number == list[i]) {
-                doesContain = true;
-            }
-        }
-        return doesContain;
-    }*/
-
 
     public static String [] userInput(int position, String [] userAnswer) {
         String usersInput;
@@ -93,6 +27,23 @@ public class Main {
         userAnswer[position - 1] = usersInput;
 
         return userAnswer;
+    }
+
+    public static void printAnswer(SecretCode secretCode, String [] userAnswer)
+    {
+        String tokenColor;
+        for(int i = 1; i < 5; i++)
+        {
+            userAnswer = userInput(i, userAnswer);
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            tokenColor = userAnswer[i];
+            System.out.print(tokenColor + ", ");
+        }
+        System.out.println("\n" +
+                secretCode);
     }
 
 }
